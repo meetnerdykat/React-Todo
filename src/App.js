f8e333138f6936b67221d6553f1239c363619609
Kathy Cuevas <meetnerdykat@gmail.com>
2019-05-14 21:56:34 -0400

    added bootstrap, google fonts, and font awesome

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import './App.css';
+import Todo from './components/TodoComponents/Todo';
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -6,8 +8,8 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <Todo />
       </div>
     );
   }
