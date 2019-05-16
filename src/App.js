9825537a1174db8351b54f37c0ec7f57b0b9a2eb
Kathy Cuevas <meetnerdykat@gmail.com>
2019-05-15 22:39:14 -0400

    created todos, todoitem, and header components and added state and toggling functionality on the todo items.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,84 @@
 import React from 'react';
 import './App.css';
-import Todo from './components/TodoComponents/Todo';
+import Todos from './components/TodoComponents/Todos';
+import Header from './components/LayoutComponents/Header';
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Pray and give thanks',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Go for a run',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Shower',
+        completed: false
+      },
+      {
+        id: 4,
+        title: 'Make smoothies',
+        completed: false
+      },
+      {
+        id: 5,
+        title: 'Take kids to school',
+        completed: false
+      },
+      {
+        id: 6,
+        title: 'Go to work',
+        completed: false
+      },
+      {
+        id: 7,
+        title: 'Go to school & code',
+        completed: false
+      }
+    ]
+  };
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+
+  // Toggle Todo item to complete or not complete
+  markComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  // Delete Todo item
+  deleteTodo = id => {
+    // this is going to filter out the one I'm deleting
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    });
+  };
+
   render() {
+    console.log(this.state.todos);
     return (
       <div className="App">
-        <Todo />
+        <Header />
+        <div className="mx-4">
+          <Todos
+            todos={this.state.todos}
+            markComplete={this.markComplete}
+            deleteTodo={this.deleteTodo}
+          />
+        </div>
       </div>
     );
   }
---
+++ src/components/LayoutComponents/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="mb-4 p-2 bg-dark text-center">
+      <h1 className="font-weight-bold">React Todo App</h1>
+    </header>
+  );
+}
+
+export default Header;
